4e452bd10478979ba98e994f4cef0dd687a91f9a
luiz frança <kazumi200516@gmail.com>
2025-02-11 14:26:24 -0300

    new cards page

---
+++ Kaiji-wiki/public/script/script.js
@@ -0,0 +1,15 @@
+function scrollcardtesoura() {
+    document.getElementById('tesoura').scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
+function scrollcardpapel() {
+    document.getElementById('papel').scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
+function scrollcardpedra() {
+    document.getElementById('pedra').scrollIntoView({
+        behavior: 'smooth' 
+    });
+}
