d53ca35bb2ada6f7995ebf6e26c25d69156971ba
luizfrz <luizfranca2005@gmail.com>
2025-04-26 12:39:51 -0300

    Docs: Alterando jeito de organizar arquivo css

--- Kaiji-wiki/public/menu/include/sugestao-game-forms/php/sugestao.php
+++ Kaiji-wiki/public/menu/include/sugestao-game-forms/php/sugestao.php
@@ -1,5 +1,15 @@
-
 <?php
- 
+$serverIp =  '';
+$user = '';
+$password =  '';
+$database = '';
+
+$connection_data = new mysqli($serverIp, $user, $password, $database);
+
+mysqli_report(MYSQLI_REPORT_OFF);
+
+if($mysqli->connect_errno) {
+    error_log('erro de conexao: ' . $mysqli->connect_errno);
+};
 ?>
    
