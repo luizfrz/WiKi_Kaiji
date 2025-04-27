67a70dcd989c8020c7d5d7dcfe2d75c3c19a9e1c
luiz frança <126346291+luizfrz@users.noreply.github.com>
2025-04-27 11:39:09 -0300

    Update script.js
    script organizando

--- Kaiji-wiki/src/script.js
+++ Kaiji-wiki/src/script.js
@@ -11,19 +11,19 @@ function scrollcardtesoura() {
         elemento.classList.remove('zoom-effect');
     }, 1000);
 }
+
 function scrollcardpapel() {
     let elemento = document.getElementById('papel');
 
     elemento.scrollIntoView({
         behavior: 'smooth'
     });
-
     elemento.classList.add('zoom-effect');
-
     setTimeout(() => {
         elemento.classList.remove('zoom-effect');
     }, 1000);
 }
+
 function scrollcardpedra() {
     let elemento = document.getElementById('pedra');
 
@@ -37,18 +37,21 @@ function scrollcardpedra() {
         elemento.classList.remove('zoom-effect');
     }, 1000);
 }
+
 function selecionadopapel() {
   const papel = document.getElementById('Papel-select');
   papel.addEventListener('click',   
     alert(" JOGADOR 1: voce jogou pedra")
   );
 }
+
 function selecionadopedra() {
     const papel = document.getElementById('Pedra-select');
     papel.addEventListener('click',   
       alert("JOGADOR 1: voce jogou papel")
     );
   }
+
   function selecionadotesoura() {
     const papel = document.getElementById('Tesoura-select');
     papel.addEventListener('click',   
